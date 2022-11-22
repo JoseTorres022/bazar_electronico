c4bb7a02763e52460c72c26c647aaaedf530c82e
JoseTorres022 <joseangel.tosa@outlook.com>
2022-11-22 09:05:40 -0700

    reestrucutra del sistema bazar electronico

---
+++ Config/configuracion.php
@@ -0,0 +1,22 @@
+<?php 
+
+const BASE_URL="http://localhost/bazar_electronico";
+
+
+//Zona horioa
+date_default_timezone_get('America/Mexico');
+
+//datos de conexion a la base de datos
+const DB_HOST="localhost";
+const DB_NAME="bazar_electronico";
+const DB_USER="josetorresxampp";
+const DB_PASSWORD="123456789";
+const DB_CHARSET="charset=utf8";
+
+//delimitadores decimal y millar.
+const SPD=".";
+const SPM=",";
+
+///simbolo de moneda
+const SMONEY="$";
+?>
