1ce933498584a8179a30f1a0347fb9b3fd0eec49
JoseTorres022 <joseangel.tosa@outlook.com>
2022-11-30 08:43:03 -0700

    mostrar roles con jason, no muestra los datos

--- Assests/js/functions_roles.js
+++ Assests/js/functions_roles.js
@@ -1,5 +1,30 @@
+var tableRoles;
+document.addEventListener("DOMContentLoaded", function () {
+    tableRoles = $('#tableRoles').DataTable({
+        "aProcessing": true,
+        "aServerSide": true,
+        "language": {
+            "url": "//cdn.datatables.net/plug-ins/1.12.1/i18n/es-ES.json"
+        },
+        "ajax": {
+            "url": " " + base_url + "/Roles/getRoles",
+            "dataSrc": ""
+        },
+        "columns": [
+            { "data": "idrol" },
+            { "data": "nombrerol" },
+            { "data": "descripcion" },
+            { "data": "status" }
+        ],
+        "resonsieve": "true",
+        "bDestroy": true,
+        "iDisplayLength": 10,
+        "order": [[0, "desc"]]
+    });
+});
+
 $('#tableRoles').DataTable();
 
-function openModal(){
+function openModal() {
     $('#modalFormRol').modal('show');
 }
